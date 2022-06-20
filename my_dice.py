50d0806702c268e3d23b93d8a1f6c2ecd95735e6
sam dare <samdr000@gmail.com>
2022-06-20 02:54:25 -0500

    updated master branch with current rev3 file

--- my_dice.py
+++ my_dice.py
@@ -1,53 +1,99 @@
-'''
-rev0.1
-'''
+#rev3
+import datetime
+import csv
 
-# usage: my_dice.py [-h] [--sum] N [N ...]
-'''class Dice():
-    def __init__(self, polyhedral_count = 6):
-        self.r = polyhedral_count = 6
-        self.my_list = [] 
-    #polyhedral_count = 6
-   # my_list = []'''
-def get_set(polyhedral_count, my_list):
-    
+#generic function for loop
+def my_range(start, end, step):
+    while start <= end:
+        yield start
+        start += step
+
+# displaying the contents of the CSV file
+def get_row_from_file(filename, polyhedral_count):
+    try:
+        with open(filename, mode ='r', newline='') as csvfile:   
+            sumreader = csv.reader(csvfile, delimiter=",")   #, quotechar="|"
+            for x in sumreader:
+                row = x
+        return row
+    except FileNotFoundError:     #if no file create and empty one
+        print("no file")
+        with open(filename, 'w', newline='') as csvfile:
+            empty_string = [str(datetime.datetime.now())]
+            empty_string.append(polyhedral_count)
+            for i in my_range(1, polyhedral_count, 1):
+                empty_string.append('0')
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(empty_string)
+        return empty_string            
+# writing to csv file
+def write_to_file(filename, data_row):
+
+    with open(filename, 'w', newline='') as csvfile:
+        # creating a csv writer object
+        csvwriter = csv.writer(csvfile)
+        # writing the data rows
+        csvwriter.writerow(data_row)
+
+def get_set(polyhedral_count):
+    my_list = []
     while True:
         value = input("Enter a number: ")
         #end on done
         #add options to remove last input
         if value == "done" :
             break
+        if value == "r" or value == "re" or value == "remove":
+            my_list = my_list[:-1]     #slice item -1 off the end
+            continue
         try:
             value = int(value)
             if value > polyhedral_count:
                 print(f"{value} is greater than {polyhedral_count}")
                 continue
             elif value <= 0:
-                print("cannot be less than zero")
+                print("cannot be less than one")
             else:
                 my_list.append(value)
-
         except:
             print("Invalid input:")
             continue
     return my_list
 
-def my_range(start, end, step):
-    while start <= end:
-        yield start
-        start += step
+def sort_set(polyhedral_count, my_sort_list, file_row):
+    result_list = []
+    for i in my_range(1, polyhedral_count, 1): 
+        x = my_sort_list.count(i)
+        y = file_row[i - 1]
+        result_of_adding_new_count_with_old_count = x + y
+        result_list.append(str(result_of_adding_new_count_with_old_count))
+    return result_list
 
-def sort_set(polyhedral_count, my_list):
-    for i in my_range(1, polyhedral_count, 1):
-            print(f"{i}: {my_list.count(i)}")
+def display_data(cnt, dta):
+    for n in my_range(1, cnt, 1):
+        new_dta = dta[2:]   #strp date and poly count off
+        print(f"{n}: {new_dta[n-1]}") 
 
 def main():
-    '''dice1=Dice()
-    dice1.get_set()'''
-    #sort_set(get_set(6)
-    polyhedral_count=7
-    my_list=[]
-    new_list = get_set(polyhedral_count, my_list)
-    #print(new_list)
-    sort_set(polyhedral_count, my_list)
+    
+    polyhedral_count=6
+    filename='DiceDATA.csv'
+
+    print(f"\t loaded:  {get_row_from_file(filename, polyhedral_count)}")
+
+    #build the row to be written to the file
+    result_list=get_set(polyhedral_count)
+    file_data_row=[str(datetime.datetime.now())]
+    file_data_row.append(str(polyhedral_count))
+    old_row=get_row_from_file(filename, polyhedral_count)
+
+    new_row=old_row[2:]
+    new_int_row=list(map(int, new_row))   #cast int to list so it can be written
+
+    file_data_row.extend(sort_set(polyhedral_count, result_list, new_int_row))
+
+    write_to_file(filename, file_data_row) 
+
+    display_data(polyhedral_count, file_data_row)
+    
 main()
